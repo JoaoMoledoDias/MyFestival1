c77c00173abb3999095fc04aa015b5b857d4e327
JoaoMoledoDias <40269391+JoaoMoledoDias@users.noreply.github.com>
2019-03-21 08:58:46 +0000

    Inserção de Toast nos Butoões do Menu

--- app/src/main/java/pt/ipg/myfestival/MainActivity.java
+++ app/src/main/java/pt/ipg/myfestival/MainActivity.java
@@ -3,6 +3,8 @@ package pt.ipg.myfestival;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+
+    public void displayToast(View view) {
+        Toast.makeText(MainActivity.this,"0 resultados encontrados",Toast.LENGTH_LONG).show();
+    }
+
+    public void displayToast1(View view) {
+        Toast.makeText(MainActivity.this,"Lista de Festivais realizados pela empresa",Toast.LENGTH_LONG).show();
+    }
+
+    public void displayToast2(View view) {
+        Toast.makeText(MainActivity.this,"Funcionarios disponiveis para a época",Toast.LENGTH_LONG).show();
     }
 }
