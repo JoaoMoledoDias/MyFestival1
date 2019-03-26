b2a0a4fa717372cd96cf2c54638b2e1ceb8d8ff8
JoaoMoledoDias <40269391+JoaoMoledoDias@users.noreply.github.com>
2019-03-26 15:38:50 +0000

    Butão Adicionar Novo Festival para nova Atividade

--- app/src/main/java/pt/ipg/myfestival/Festivais.java
+++ app/src/main/java/pt/ipg/myfestival/Festivais.java
@@ -2,13 +2,33 @@ package pt.ipg.myfestival;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Festivais extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_festivais);
+
+        button = findViewById(R.id.butaoAdicionarFes);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                opennadicionarFestival();
+            }
+        });
     }
+
+    public void opennadicionarFestival(){
+        Intent intent = new Intent(this,adicionarFestival.class);
+        startActivity(intent);
+    }
+
+
+    
 }
---
+++ app/src/main/java/pt/ipg/myfestival/adicionarFestival.java
@@ -0,0 +1,14 @@
+package pt.ipg.myfestival;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class adicionarFestival extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adicionar_festival);
+    }
+}
