50ff20551a87478259926d7d0b107ea438b8b1ba
JoaoMoledoDias <40269391+JoaoMoledoDias@users.noreply.github.com>
2019-03-26 16:55:56 +0000

    Campos Obrigatorios e função finish() butao Guardar

--- app/src/main/java/pt/ipg/myfestival/adicionarFestival.java
+++ app/src/main/java/pt/ipg/myfestival/adicionarFestival.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class adicionarFestival extends AppCompatActivity {
     private Button button;
@@ -23,4 +25,36 @@ public class adicionarFestival extends AppCompatActivity {
         });
 
     }
-}
+
+    public void GuardarFestival (View view){
+            EditText nomeFestival = (EditText) findViewById(R.id.nomeFestival);
+            String nomeFes = nomeFestival.getText().toString();
+
+            if (nomeFes.trim().length() == 0) {
+                nomeFestival.setError("Campo inválido");
+                nomeFestival.requestFocus();
+                return;
+            }
+
+            EditText localFestival = findViewById(R.id.localFestival);
+            String localFes = localFestival.getText().toString();
+
+        if (localFes.trim().length() == 0) {
+            localFestival.setError("Campo inválido");
+            localFestival.requestFocus();
+            return;
+        }
+
+        button = (Button) findViewById(R.id.butaoGuardarFes);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                Toast.makeText(adicionarFestival.this,"Festival Guardado com Sucesso",Toast.LENGTH_LONG).show();
+            }
+        });
+        }
+
+    }
+
+
